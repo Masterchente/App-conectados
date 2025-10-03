81f56addffc4ed4166a6aeb8a15519fb8554e0bd
Masterchente <117607857+Masterchente@users.noreply.github.com>
2025-10-03 09:48:35 -0600

    screen dashboard

--- Conectados/App.tsx
+++ Conectados/App.tsx
@@ -1,3 +1,4 @@
+// App.tsx
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -5,11 +6,15 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SplashScreen from "./src/screens/SplashScreen";
 import LoginScreen from "./src/screens/LoginScreen";
 import RegisterScreen from "./src/screens/RegisterScreen";
+import ElderlyJoin from "./src/screens/ElderlyJoin";
+import Dashboard from "./src/screens/Dashboard";
 
 export type RootStackParamList = {
   Splash: undefined;
   Login: undefined;
   Register: undefined;
+  ElderlyJoin: undefined;
+  Dashboard: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -17,13 +22,12 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Splash"
-        screenOptions={{ headerShown: false }}
-      >
+      <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="ElderlyJoin" component={ElderlyJoin} />
+        <Stack.Screen name="Dashboard" component={Dashboard} />
       </Stack.Navigator>
     </NavigationContainer>
   );
